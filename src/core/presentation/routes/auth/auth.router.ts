0f1aec7d0ff297c4a9fd23c70c173d88003a086d
RunyShark <spartadragon117@live.com.mx>
2024-02-13 21:35:08 -0600

    FEAT response repository

--- src/core/presentation/routes/auth/auth.router.ts
+++ src/core/presentation/routes/auth/auth.router.ts
@@ -2,12 +2,18 @@ import { Request, Response } from 'express';
 import { Router } from 'express';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import {
+  AuthDataSourceMongo,
+  AuthRepositoryImpl,
+} from 'src/core/infrastructure';
 
 export class AuthRouter {
   constructor(private readonly router: Router) {}
 
   get routes(): Router {
-    const authController = new AuthController(new AuthService());
+    const authRepository = new AuthRepositoryImpl(new AuthDataSourceMongo());
+
+    const authController = new AuthController(new AuthService(authRepository));
 
     this.router.post('/login', authController.login);
 
--- src/core/presentation/routes/auth/auth.service.ts
+++ src/core/presentation/routes/auth/auth.service.ts
@@ -1,16 +1,19 @@
 import { Catch } from '@decorators/Catch.decorator';
-import { ApiResponse, RegisterUserDto } from 'src/core/domain';
+import { ApiResponse, RegisterUserDto, UserEntity } from 'src/core/domain';
+import { AuthRepository } from '../../../domain/repositories/auth.repository';
 
 @Catch
 export class AuthService {
-  constructor() {}
+  constructor(private readonly authRepository: AuthRepository) {}
 
-  login(dto: RegisterUserDto) {
-    const [error, result] = RegisterUserDto.create(dto);
+  async login(dto: RegisterUserDto) {
+    const [error, registerUserDto] = RegisterUserDto.create(dto);
 
     if (error) return ApiResponse.errorHandle<string>(error);
 
-    return ApiResponse.successHandle<RegisterUserDto>(result!);
+    const result = await this.authRepository.register(registerUserDto!);
+
+    return ApiResponse.successHandle<UserEntity>(result);
   }
 
   register() {
