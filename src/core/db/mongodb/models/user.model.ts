bb555a13ef212c6e3b332ebc2144347d84bd6b85
RunyShark <spartadragon117@live.com.mx>
2024-02-14 07:58:41 -0600

    FEAT Models and MongoDB

---
+++ src/core/db/mongodb/models/user.model.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    roles: {
+      type: [String],
+      default: ['USER_ROLE'],
+      enum: ['ADMIN_ROLE', 'USER_ROLE'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const UserModel = mongoose.model('User', userSchema);
